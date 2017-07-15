361fa56232bc6ea7acb45a302cc7496b44e27be9
leenbean16 <shamoonlina@gmail.com>
2017-07-15 04:53:34 -0700

    movie

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,14 @@
+// node bamazon application.
+// variables:
 let colors = require('colors');
+let math = require('math');
 let mysql = require('mysql');
 let Table = require('cli-table');
 let inquirer = require('inquirer');
-let purchased = [];
+let cart = [];
 let command = process.argv[2]
 let secondCommand = process.argv[3]
-let cart = [];
+let receipt = [];
 
 // Connection to MySQL database.
 let connection = mysql.createConnection({
@@ -16,29 +19,32 @@ let connection = mysql.createConnection({
 });
 
 // Welcome banner.
-console.log("\n-----------------------------------------------------------".rainbow);
-console.log("                     Welcome to Bamazon.                   ");
-console.log(" Type: node BamazonCustomer.js customer/manager/supervisor.");
-console.log("-----------------------------------------------------------".rainbow);
-console.log("\n");
+function welcome() {
+    console.log("\n-----------------------------------------------------------".rainbow);
+    console.log("                     Welcome to Bamazon.                   ");
+    console.log("-----------------------------------------------------------".rainbow);
+    console.log("\n");
+};
 
+welcome();
+
+// brings up table with contents from mysql.
 let shop = function() {
     let table = new Table({
         head: ['ID', 'Product', 'Department', 'Price', 'Stock'],
         colWidths: [4, 13, 14, 13, 8]
     });
+    // connect to mysql
     connection.query('SELECT * FROM products', function(err, res) {
         for (let i = 0; i < res.length; i++) {
             table.push(
                 [res[i].itemID, res[i].ProductName, res[i].Department, res[i].Price.toFixed(2), res[i].StockQuantity]
             );
         }
-
-        // console.log(table.toString());
-        // // table
-
+        // inquirer shopping prompt
         runCommand(process.argv[2]);
 
+        // kind of a long function :/
         function runCommand(command) {
             console.log(table.toString());
             // table
@@ -72,30 +78,26 @@ let shop = function() {
                 let chosenProduct = res[chosenId]
                 let chosenQuantity = answer.Quantity
                 if (chosenQuantity < chosenProduct.StockQuantity) {
+                    // Views total cost for chosen product * amount.
                     console.log("\n-----------------------------------------------------------".rainbow);
                     console.log("\nYour total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].ProductName + " is: " + res[chosenId].Price.toFixed(2) * chosenQuantity);
                     console.log("                                                           ");
                     console.log("-----------------------------------------------------------".rainbow);
-                    cart.push(res[chosenId].ProductName + " : " + res[chosenId].Price.toFixed(2) * chosenQuantity + " ");
-                    console.log("\nYour cart:");
-                    console.log(cart);
-                    total();
-                    console.log("\n");
-
-
+                    // push items to receipt
+                    receipt.push(res[chosenId].ProductName + " : " + res[chosenId].Price.toFixed(2) * chosenQuantity + " ");
+                    cart.push(res[chosenId].Price.toFixed(2) * chosenQuantity);
+                    //Update stock.
                     connection.query("UPDATE products SET ? WHERE ?", [{
                         StockQuantity: chosenProduct.StockQuantity - chosenQuantity
                     }, {
                         itemID: chosenProduct.itemID
                     }], function(err, res) {
-                        //console.log(err);
-                        // shop();
-                        // ^ Brings up itemID prompt again.
                         shopOrExit()
                     });
                 } else {
+                    // This displays when stock is too low.
                     console.log("\n-----------------------------------------------------------".rainbow);
-                    console.log("\nSorry, we don'd have enough " + res[chosenId].ProductName + "'s" + " in our Inventory.");
+                    console.log("\nSorry, our stock of " + res[chosenId].ProductName + "'s" + " is too low right now.");
                     console.log("                                                           ");
                     console.log("-----------------------------------------------------------".rainbow);
                     shop()
@@ -105,24 +107,30 @@ let shop = function() {
     });
 }
 
-function total() {
-    var total = 0;
-    for (var i = 0; i < total.length; i++) {
-        total += cart[i] << 0;
-        console.log(total);
-    }
-}
-
 function shopOrExit() {
     inquirer.prompt([{
         // What does the customer want to do now?
         type: "list",
         message: "What do you want to do?",
-        choices: ["Keep Shopping", "Exit"],
+        choices: ["Keep Shopping", "Receipt", "Cart", "Exit"],
         name: "choice",
     }]).then(function(inquirerResponse) {
         if (inquirerResponse.choice === "Keep Shopping") {
             shop()
+        } else if (inquirerResponse.choice === "Receipt") {
+            console.log("\nYour receipt.");
+            console.log(receipt);
+            console.log("Total: $" + Math.sum(cart));
+
+            console.log("\n");
+            shopOrExit()
+        } else if (inquirerResponse.choice === "Cart") {
+            console.log("\n-----------------------------------------------------------".rainbow);
+            console.log("Your cart consists of: ");
+            console.log(receipt);
+            console.log("                                                           ");
+            console.log("-----------------------------------------------------------".rainbow);
+            shopOrExit()
         } else if (inquirerResponse.choice === "Exit") {
             console.log("\n-----------------------------------------------------------".rainbow);
             console.log("\n                   Thank you. Come again.");
@@ -131,7 +139,6 @@ function shopOrExit() {
         }
     });
 }
-
 
 shop();
 
